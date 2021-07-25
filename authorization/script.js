0bdbf6363fdf376041704b443d98855f28f392e4
AnnaKobyakova <aakobyakova_1@edu.hse.ru>
2021-07-25 14:36:23 +0300

    auth form layout

---
+++ authorization/script.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            login: '',
+            password: '',
+        }
+    },
+
+    methods: {
+        auth: function() {
+            let data = {
+                login: this.login,
+                password: this.password,
+            }
+        }
+    }
+});
+
+const vm = app.mount('#app');
