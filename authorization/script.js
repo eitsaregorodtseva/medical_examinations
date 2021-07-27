025aa68ee7785a31e3987270dcf0966eed51f76a
AnnaKobyakova <aakobyakova_1@edu.hse.ru>
2021-07-27 13:39:05 +0300

    fix colors

--- authorization/script.js
+++ authorization/script.js
@@ -12,6 +12,16 @@ const app = Vue.createApp({
                 login: this.login,
                 password: this.password,
             }
+        },
+
+        overBttn: function() {
+            let btn = document.getElementById('btn');
+            btn.style.background = 'linear-gradient(to left, #3C61E9, #1942DB)';
+        },
+
+        outBttn: function() {
+            let btn = document.getElementById('btn');
+            btn.style.background = 'linear-gradient(to left, #5F7EF1, #3C61E9)';
         }
     }
 });
