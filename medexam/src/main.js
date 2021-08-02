4fa0b0f1205d0e661e86d5ad118ecb9fcb96ef84
AnnaKobyakova <aakobyakova_1@edu.hse.ru>
2021-08-02 13:56:15 +0300

    add dependencies chart.js and popper

--- medexam/src/main.js
+++ medexam/src/main.js
@@ -3,6 +3,61 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import {
+    Chart,
+    ArcElement,
+    LineElement,
+    BarElement,
+    PointElement,
+    BarController,
+    BubbleController,
+    DoughnutController,
+    LineController,
+    PieController,
+    PolarAreaController,
+    RadarController,
+    ScatterController,
+    CategoryScale,
+    LinearScale,
+    LogarithmicScale,
+    RadialLinearScale,
+    TimeScale,
+    TimeSeriesScale,
+    Decimation,
+    Filler,
+    Legend,
+    Title,
+    Tooltip,
+    SubTitle
+  } from 'chart.js';
+  
+  Chart.register(
+    ArcElement,
+    LineElement,
+    BarElement,
+    PointElement,
+    BarController,
+    BubbleController,
+    DoughnutController,
+    LineController,
+    PieController,
+    PolarAreaController,
+    RadarController,
+    ScatterController,
+    CategoryScale,
+    LinearScale,
+    LogarithmicScale,
+    RadialLinearScale,
+    TimeScale,
+    TimeSeriesScale,
+    Decimation,
+    Filler,
+    Legend,
+    Title,
+    Tooltip,
+    SubTitle
+  );
+import Popper from "vue3-popper";
 //import 'materialize-css/dist/js/materialize.min'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).component('Popper', Popper).mount('#app')
