0a49e3ab01ec60520ddfe591fedcc33c98711e86
AnnaKobyakova <aakobyakova_1@edu.hse.ru>
2021-08-14 11:32:43 +0300

    add adaptivity for dig sig int

--- src/views/DigitalSignature.vue
+++ src/views/DigitalSignature.vue
@@ -1,14 +1,24 @@
 <template>
-    <div id="ds_rect" :class="isError ? 'app_error' : 'app_normal'">
-        <form @submit.prevent='verf'>
-            <div id='img_cont'>
-                <img id='img'  v-if="isError" src='@\assets\images\flash-drive(1).png' />
-                <img id='img' v-else src='@\assets\images\flash-drive.png' />
+    <div id="ds_rect" class='container position-absolute top-50 start-50 translate-middle' :class="isError ? 'app_error' : 'app_normal'">
+        <form class='form-horizontal' @submit.prevent='verf'>
+            <div class='form-group'>
+                <div id='img_cont' class='row mt-3 d-flex justify-content-center'>
+                    <img id='img'  v-if="isError" src='@\assets\images\flash-drive(1).png' />
+                    <img id='img' v-else src='@\assets\images\flash-drive.png' />
+                </div>
+                <div class='row d-flex justify-content-center'>
+                    <div id='err_txt' v-if="isError" class='txt_error'>Неверный ключ</div>
+                </div>
+                <div class='row d-flex justify-content-center'>
+                    <div id='text_ds' :class="isError ? 'txt_error' : 'txt_normal'">Присоедините к компьютеру носитель ключа электронной подписи.</div>
+                </div>
+                <div class='row mt-3 d-flex justify-content-center'>
+                    <button id='btn' :class="isError ? 'btn_error' : 'btn_normal'" type='submit'>Готово</button>
+                </div>
+                <div class='row d-flex justify-content-center'>
+                    <button id='nav' :class="isError ? 'nav_btn_error' : 'nav_btn_normal'" @click='back'>Назад</button>
+                </div>
             </div>
-            <div id='err_txt' v-if="isError" class='txt_error'>Неверный ключ</div>
-            <div id='text_ds' class='txt_normal'>Присоедините к компьютеру носитель ключа электронной подписи.</div>
-            <button id='btn' :class="isError ? 'btn_error' : 'btn_normal'" type='submit'>Готово</button>
-            <button id='nav' :class="isError ? 'nav_btn_error' : 'nav_btn_normal'" @click='back'>Назад</button>
         </form>
     </div>
 </template>
@@ -29,23 +39,18 @@ export default {
 <style scoped>
 
 #ds_rect {
-    position: absolute;
     width: 550px;
     height: 400px;
     display: block;
-    top: 50%;
-    left: 50%;
-    margin-left: -300px;
-    margin-top: -225px;
 }
 
+.row {
+    height: 100px;
+}
 
 #text_ds {
-    position: absolute;
     text-align: center;
-    top: 52%;
     width: 70%;
-    padding-left: 15%;
     font-size: 18px;
     font-weight: 600;
     letter-spacing: -0.017em;
@@ -54,41 +59,27 @@ export default {
 #img {
     width: 129px;
     height: 129px;
-}
-
-#img_cont {
-    position: absolute;
-    left: 50%;
-    margin-left: -64.5px;
-    top: 7%;
+    padding: 0;
 }
 
 #err_txt {
-    position: absolute;
     text-align: center;
-    top: 43%;
     width: 100%;
     font-weight: 600;
+    padding-top: 50px;
 }
 
 #nav {
-    position: absolute;
     text-align: center;
     width: auto;
-    top: 110%;
-    left: 46%;
-    /* font-weight: 00; */
+    height: 30%;
     background-color: #FFF;
 }
 
 #btn {
-    position: absolute;
-    width: 25%;
-    height: 10%;
-    top: 80%;
-    left: 37.5%;
+    height: 40%;
+    width: 30%;
+    border-radius: 50px;
 }
-
-
 
 </style>
