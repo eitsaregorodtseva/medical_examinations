91fc7b370e2957f681215fa38647b11181c0ae5f
Elizaveta Tsaregorodtseva <eitsaregorodtseva@miem.hse.ru>
2022-11-23 17:16:08 +0300

    Exam and terminal cards

---
+++ src/views/ExamCard.vue
@@ -0,0 +1,124 @@
+<template>
+  <q-card 
+    class="col-4" 
+    style="min-width: 380px; max-width: 380px;"
+  >
+    <q-item>
+      <q-item-section>
+        <q-item-label>Название организации</q-item-label>
+      </q-item-section>
+    </q-item>
+    <q-separator />
+    <q-card-section>
+      <div class="row">
+        <div class="q-pa-md col q-gutter-md">
+          <q-card class="q-pa-sm q-pl-md q-gutter-xs item-card">
+            <div>План</div>
+            <div class="row">
+              <div class="col numbers">
+                <div>0</div>
+              </div>
+              <div class="col q-ml-xl">
+                <q-icon 
+                  size="30px"
+                  name="img:/img/icons/plan.svg"
+                />
+              </div>
+            </div>
+          </q-card>
+          <q-card class="q-px-sm q-pt-lg q-pb-lg q-gutter-xs item-card">
+            <div style="font-size: 20px; font-weight: 600; text-align: center">
+              Осмотров
+            </div>
+            <div class="row q-pt-lg q-pb-sm">
+              <div 
+                class="col q-mt-md" 
+                style="font-size: 20px; font-weight: 600; text-align: center"
+              >
+                <div>0</div>
+              </div>
+              <div class="col">
+                <q-icon 
+                  size="50px"
+                  name="img:/img/icons/done.svg"
+                />
+              </div>
+            </div>
+          </q-card>
+          <q-card class="q-pa-sm q-gutter-xs item-card">
+            <div>Допуски</div>
+            <div class="row">
+              <div class="col numbers">
+                <div>0</div>
+              </div>
+              <div class="col q-ml-xl">
+                <q-icon 
+                  size="30px"
+                  name="img:/img/icons/ok.svg"
+                />
+              </div>
+            </div>
+          </q-card>
+        </div>
+        <div class="q-pa-md col q-gutter-md">
+          <q-card class="q-pa-sm q-gutter-xs item-card">
+            <div>Недопуск, всего</div>
+            <div class="row">
+              <div class="col numbers">
+                <div>0</div>
+              </div>
+              <div class="col q-ml-xl">
+                <q-icon 
+                  size="30px"
+                  name="img:/img/icons/denied.svg"
+                />
+              </div>
+            </div>
+          </q-card>
+          <q-card class="q-pa-sm q-gutter-xs item-card">
+            <div>Алкоголь</div>
+            <div class="row">
+              <div class="col numbers">
+                <div>0</div>
+              </div>
+              <div class="col q-ml-xl">
+                <q-icon 
+                  size="30px"
+                  name="img:/img/icons/alcohol.svg"
+                />
+              </div>
+            </div>
+          </q-card>
+          <q-card class="q-pa-sm q-gutter-xs item-card">
+            <div>Давление/пульс</div>
+            <div class="row">
+              <div class="col numbers">
+                <div>0</div>
+              </div>
+              <div class="col q-ml-xl">
+                <q-icon 
+                  size="35px"
+                  name="img:/img/icons/pressure.svg"
+                />
+              </div>
+            </div>
+          </q-card>
+          <q-card class="q-pa-sm q-gutter-xs item-card">
+            <div>Прочее</div>
+            <div class="row">
+              <div class="col numbers">
+                <div>0</div>
+              </div>
+              <div class="col q-ml-xl">
+                <q-icon 
+                  size="30px"
+                  name="img:/img/icons/other.svg"
+                />
+              </div>
+            </div>
+          </q-card>
+        </div>
+      </div>
+    </q-card-section>
+  </q-card>
+</template>
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,8 +1,74 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <q-page padding>
-    <div class="row q-gutter-md col-6">
+  <q-page>
+    <div class="q-pa-md q-ml-xl">
+      <q-tabs 
+        v-model="tab" 
+        dense 
+        active-color="white" 
+        active-bg-color="dark"
+        active-radius="20px" 
+        indicator-color="transparent" 
+        align="justify"
+        style="width: 90%"
+      >
+        <q-tab name="main" label="Главная"></q-tab>
+        <q-tab name="documents" label="Документы"></q-tab>
+        <q-tab name="1c" label="1С"></q-tab>
+        <q-tab name="medworkers" label="Медики"></q-tab>
+        <q-tab name="applications" label="Заявки"></q-tab>
+        <q-tab name="settings" label="Настройки"></q-tab>
+        <q-tab name="help" label="Справка"></q-tab>
+      </q-tabs>
+      
+      <q-tab-panels v-model="tab">
+        <q-tab-panel name="main">
+          <div class="q-py-md row">
+            <q-btn-toggle
+              v-model="model" 
+              toggle-color="dark" 
+              :options="[
+                {label: 'Сегодня', value: 'today'},
+                {label: 'Месяц', value: 'month'},
+                {label: 'Год', value: 'year'}
+              ]" 
+              style="border-radius: 5px"
+            />
+            <!-- <q-input 
+              v-model="search" 
+              type="search" 
+              label="Поиск"
+              style="float: right;"
+            >
+              <template 
+                v-slot:append
+              >
+                <q-icon name="search" />
+              </template>
+            </q-input> -->
+          </div>
+          <div 
+            class="q-py-xl row q-gutter-xl" 
+            style="justify-content: center;"
+          >
+            <exam-card />
+            <terminal-card />
+          </div>
+        </q-tab-panel>
+        <q-tab-panel name="documents" />
+        <q-tab-panel name="1c" />
+        <q-tab-panel name="medworkers" />
+        <q-tab-panel name="applications" />
+      
+        <q-tab-panel name="settings" />
+      
+        <q-tab-panel name="help" />
+      </q-tab-panels>
+    </div>
+      
+    <!-- <div class="row q-gutter-md col-6">
       <div class="h6 col">Выберите период</div>
-      <!-- <q-date class="col" v-model="model" range minimal /> -->
+      <q-date class="col" v-model="model" range minimal />
       <input
         v-model="dates.from"
         class="col"
@@ -23,26 +89,41 @@
       :dates="dates"
       :user_id="user_id"
       height="80vh"
-    />
+    /> -->
   </q-page>
 </template>
 
 <script>
-import OrganizationsStatisticsTable from '@/components/OrganizationsStatisticsTable.vue'
+// import OrganizationsStatisticsTable from '@/components/OrganizationsStatisticsTable.vue'
+import ExamCard from '@/views/ExamCard.vue'
+import TerminalCard from '@/views/TerminalCard.vue'
 import { getAllOrganizationsStats, getOneOrganizationStats } from '@/api/organizations.api.js'
 import moment from 'moment'
+import { ref } from 'vue'
 
 export default {
   components : {
-        OrganizationsStatisticsTable
+        // OrganizationsStatisticsTable,
+        ExamCard,
+        TerminalCard
     },
-  data () {return {
+    // setup() {
+    //   return {
+    //     tab: ref("main"),
+    //     model: ref("one"),
+    //   }
+    // },
+  data () {
+    return {
     // model: {from: '2020/07/07', to: '2020/07/17' }
+    tab: ref("main"),
+    model: ref("today"),
     dates: {from: moment().subtract(1, 'months').format('YYYY-MM-DD'), to: moment().format('YYYY-MM-DD')},
     user_id: null,
     user_organization_id: null,
     organizationsList : [],
     showTable: false,
+    
   }},
   mounted() {
         this.populateDataFromStorage()
@@ -63,6 +144,7 @@ export default {
               response = await getOneOrganizationStats(this.user_organization_id, this.user_id, this.dates.from, this.dates.to)
             }
             this.organizationsList = response.data
+            console.log(response.data)
           } catch (err) {
             console.log(err)
           }
@@ -73,6 +155,28 @@ export default {
 }
 </script>
 
-<style>
-
+<style> 
+  .q-tabs--dense .q-tab {
+    border-radius: 5px;
+    text-transform: capitalize;
+  }
+  .q-tab-panel {
+    padding-left: 0px;
+  }
+  .q-btn {
+    text-transform: capitalize;
+  }
+  .q-btn .q-focus-helper {
+    display: none;
+  }
+  .btn {
+    cursor: auto;
+  }
+  .numbers {
+    text-align: center;
+    font-size: 20px;
+  }
+  .item-card {
+    border-radius: 10px;
+  }
 </style>
