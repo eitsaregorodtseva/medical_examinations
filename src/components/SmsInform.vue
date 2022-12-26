8870aa59d4803f2aaa9d06ed18d7788dd4950205
Elizaveta Tsaregorodtseva <eitsaregorodtseva@miem.hse.ru>
2022-12-26 23:55:24 +0300

    api methods +

--- src/components/SmsInform.vue
+++ src/components/SmsInform.vue
@@ -1,89 +1,128 @@
 <template>
-  <div class="q-pa-lg" style="max-width: 500px">
+  <div class="q-pa-lg q-ml-xl" style="max-width: 600px">
+    <div class="q-mb-lg" style="font-weight: 700; font-size: 16px;">
+      Согласие на СМС-информирование
+    </div>
     <q-form @submit="submitForm">
-      <q-input
-        v-model="person.last_name"
-        :disable="change_state"
-        label="Фамилия *"
-        lazy-rules
+      <q-input 
+        v-model="person.second_name" 
+        :disable="change_state" 
+        label="Фамилия *" 
+        lazy-rules 
         :rules="[
-          (val) => (val && val.length > 0) || 'Пожалуйста, заполните это поле',
-        ]"
+          (val) => (val && val.length > 0) || 'Пожалуйста, заполните это поле' ]" 
       />
-      <q-input
-        v-model="person.first_name"
-        :disable="change_state"
-        label="Имя *"
-        lazy-rules
+      <q-input 
+        v-model="person.first_name" 
+        :disable="change_state" 
+        label="Имя *" 
+        lazy-rules 
         :rules="[
-          (val) => (val && val.length > 0) || 'Пожалуйста, заполните это поле',
-        ]"
+          (val) => (val && val.length > 0) || 'Пожалуйста, заполните это поле' ]" 
       />
-      <q-input
-        v-model="person.patronymic"
-        :disable="change_state"
-        label="Отчество *"
-        lazy-rules
-        :rules="[
-          (val) => (val && val.length > 0) || 'Пожалуйста, заполните это поле',
-        ]"
+      <q-input 
+        v-model="person.father_name" 
+        :disable="change_state" 
+        label="Отчество (при наличии)" 
+        lazy-rules 
       />
-      <q-select
-        v-model="person.organization"
-        :disable="change_state"
-        :options="organizations"
+      <q-select 
+        v-model="person.organization_id" 
+        :disable="change_state" 
+        :options="organizations" 
         label="Организация *"
-        lazy-rules
-        :rules="[(val) => val || 'Пожалуйста, выберите организацию из списка']"
+        lazy-rules 
+        :rules="[(val) => val || 'Пожалуйста, выберите организацию из списка']" 
+        style="margin-top: 24px" 
       />
-      <q-input
-        v-model="person.phone_number"
-        :disable="change_state"
-        type="tel"
-        label="Номер телефона *"
+      <q-input 
+        v-model="person.tel" 
+        :disable="change_state" 
+        type="tel" 
+        label="Номер телефона *" 
         lazy-rules
-        mask="+7(###)###-##-##"
-        fill-mask
+        mask="+7 (###) ###-##-##" 
+        fill-mask 
         :rules="[
-          (val) => (val.length === 16) || 'Пожалуйста, заполните это поле',
-        ]"
+          (val) => (/^(\+7)?[\s\-]?\(?[0-9]{3}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/).test(val) || 'Пожалуйста, заполните это поле']" 
       />
-      <q-checkbox
-        v-model="agreement"
-        :disable="change_state"
-        label="Я соглашаюсь на обработку персональных данных и получение смс-уведомлений по этому номеру»"
+      <q-checkbox 
+        v-model="agreement" 
+        :disable="code_sent"
+        label="Я соглашаюсь на обработку персональных данных и получение смс-уведомлений по этому номеру" 
       />
-      <q-btn 
-        v-if="!code_sent" 
-        :disable="agreement ? false : true" 
-        color="dark"
-        type="submit"
-      >
+      <!-- <div class="row q-gutter-sm">
+        <q-input outlined style="width: 50px" mask="#"></q-input>
+        <q-input outlined style="width: 50px" mask="#"></q-input>
+        <q-input outlined style="width: 50px" mask="#"></q-input>
+        <q-input outlined style="width: 50px" mask="#"></q-input>
+      </div> -->
+      <q-btn v-if="!code_sent" class="q-mt-lg" :disable="agreement ? false : true" color="dark" type="submit">
         Отправить код подтверждения
       </q-btn>
+
       <div v-if="code_sent">
-        <q-btn v-if="!change_state" @click="saveChanges">Сохранить изменения</q-btn>
-        <q-btn v-if="change_state" @click="openForm">Изменить данные</q-btn>
-        <q-input></q-input>
-        <q-btn>Отправить код еще раз</q-btn>
+        <div v-if="!change_state" class="q-gutter-md">
+          <q-btn class="q-mt-lg" color="dark" type="submit">
+            Сохранить изменения
+          </q-btn>
+          <q-btn class="q-mt-lg" @click="cancelChanges">
+            Отменить
+          </q-btn>
+        </div>
+        <q-btn v-if="change_state" class="q-mt-lg" color="dark" @click="openForm">
+          Изменить данные
+        </q-btn>
+        <div v-if="code_sent && change_state">
+          <q-input v-model="code" mask="####"></q-input>
+          <q-btn @click="approveCode">Отправить код</q-btn>
+          <div class="row q-mt-md q-gutter-sm">
+            <q-btn :disable="timer.seconds !== '00'" class="q-mt-md" color="dark" @click="resendCode">
+              Отправить код еще раз
+            </q-btn>
+            <div class="q-pt-md" style="font-weight: 600">
+              {{ timer.minutes }}:{{ timer.seconds }}
+            </div>
+          </div>
+        </div>
       </div>
     </q-form>
   </div>
 </template>
 <script>
 import { ref } from "vue";
+import { Notify } from 'quasar';
 import { getAllOrganizations } from "@/api/organizations.api.js";
+import {
+  postPhoneNumber,
+  getCheckCodeStatus,
+  postUser,
+} from "@/api/sms.api.js";
 
 export default {
   data() {
     return {
       person: {
-        last_name: "",
+        second_name: "",
         first_name: "",
-        patronymic: "",
-        organization: "",
-        phone_number: "",
+        father_name: "",
+        organization_id: "",
+        tel: "",
       },
+      person_last_state: {
+        second_name: "",
+        first_name: "",
+        father_name: "",
+        organization_id: "",
+        tel: "",
+      },
+      timer: {
+        minutes: "00",
+        seconds: "20",
+        interval: 0,
+      },
+      tel_cleared: '',
+      code: ref(''),
       agreement: ref(false),
       code_sent: ref(false),
       change_state: ref(false),
@@ -91,31 +130,108 @@ export default {
     };
   },
   mounted() {
-    this.populateDataFromStorage();
     this.getOrganizations();
   },
+  beforeUnmount() {
+    clearInterval(this.timer.interval)
+  },
   methods: {
-    populateDataFromStorage() {
-      this.user_id = sessionStorage.getItem("user_id");
-    },
-
-    submitForm() {
+    async submitForm() {
+      clearInterval(this.timer.interval)
       this.code_sent = true;
       this.change_state = true;
-      console.log(this.person)
+      this.timer = { ...this.timer, seconds: "20" };
+      this.startTimer(parseInt(this.timer.seconds));
+      if (this.person.tel !== this.person_last_state.tel) {
+        this.tel_cleared = this.person.tel.replace(/[+() -]/g, '');
+        console.log(this.tel_cleared)
+        try {
+          var response = await postPhoneNumber(this.tel_cleared);
+          console.log(response);
+          console.log(response.status);
+          Notify.create({
+            color: 'green-5',
+            textColor: 'white',
+            message: 'Код отправлен!'
+          });
+        } catch (err) {
+          console.log(err.response.status);
+          Notify.create({
+            color: 'red-5',
+            textColor: 'white',
+            message: 'Ошибка!'
+          });
+        }
+      }
+    },
+
+    async approveCode() {
+      try {
+        var response = await getCheckCodeStatus(this.tel_cleared, this.code);
+        console.log(response);
+        console.log(response.status);
+        if (response.status === 201) {
+          this.sendAllData();
+        }
+      } catch (err) {
+        console.log(err);
+      }
+      console.log(this.tel_cleared, this.code);
+    },
+
+    async sendAllData() {
+      try {
+        var response = await postUser(this.person.second_name, this.person.first_name, this.person.father_name, this.person.organization_id.value, this.tel_cleared);
+        console.log(response);
+        Notify.create({
+          color: 'green-5',
+          textColor: 'white',
+          message: 'Код успешно принят!'
+        })
+      } catch (err) {
+        console.log(err);
+        Notify.create({
+          color: 'red-5',
+          textColor: 'white',
+          message: 'Ошибка!'
+        });
+      }
+      console.log(this.person.second_name, this.person.first_name, this.person.father_name, this.person.organization_id.value, this.tel_cleared);
+    },
+
+    cancelChanges() {
+      this.change_state = true;
+      this.person = Object.assign({}, this.person_last_state);
     },
 
     openForm() {
-        this.change_state = false;
+      this.change_state = false;
+      this.person_last_state = Object.assign({}, this.person);
     },
 
     saveChanges() {
-        this.change_state = true;
+      this.change_state = true;
+    },
+
+    resendCode() {
+      this.submitForm();
+    },
+
+    startTimer(duration) {
+      var timer = duration
+      this.timer.interval = setInterval(() => {
+        this.timer.seconds = parseInt(timer % 60, 10)
+        this.timer.seconds = this.timer.seconds < 10 ? "0" + this.timer.seconds : this.timer.seconds
+        if (--timer < 0) {
+          timer = 0
+          clearInterval(this.timer.interval)
+        }
+      }, 1000)
     },
 
     async getOrganizations() {
       try {
-        var response = await getAllOrganizations(this.user_id);
+        var response = await getAllOrganizations();
         this.organizations = response.data.map((obj, index) => {
           return { value: obj.id, label: obj.name };
         });
@@ -126,3 +242,8 @@ export default {
   },
 };
 </script>
+<style>
+.q-btn {
+  text-transform: none;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,7 @@ const routes = [
       } else {
         if (role == Role.MedWorker){
           return '/new_exams'
-        }
+        } 
         else{
           return '/exams_history'
         }
@@ -113,8 +113,8 @@ const routes = [
     path: '/sms',
     name: 'Sms',
     component: () => import('../components/SmsInform.vue'),
-    meta :{
-      
+    meta: {
+      layout: 'auth'
     }
   }
 ]
@@ -125,7 +125,10 @@ const router = createRouter({
 })
 
 router.beforeEach((to, from, next) => {
-  if (!sessionStorage.getItem('full_auth') && !(to.path === '/auth' || to.path === '/dig_sig')) {
+  if (
+    !sessionStorage.getItem('full_auth') &&
+    !(to.path === '/auth' || to.path === '/dig_sig' || to.path === '/sms')
+  ) {
     // the user is not logged in, redirect to auth page
     next({ name: 'Auth', query: { returnUrl: to.path } })
   } else if (to.meta.permittedRoles) {
