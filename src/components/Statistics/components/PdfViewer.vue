425a0ad7aeb549b65712d04fbb43b5af9839819c
Elizaveta Tsaregorodtseva <eitsaregorodtseva@miem.hse.ru>
2023-04-26 20:38:41 +0300

    docs

---
+++ src/components/Statistics/components/PdfViewer.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    class="q-mt-md"
+    style="display: flex; justify-content: center;"
+  >
+    <div
+      style="max-height: 65vh; overflow: auto;"
+    >
+      <pdf
+        style="width: 90%;"
+        :src="path"
+      />
+    </div>
+  </div>
+</template>
+<script>
+import pdf from 'pdfvuer';
+export default {
+  components: {
+    pdf,
+  },
+  props: {
+    path: {
+      type: String,
+      default: ''
+    }
+  },
+};
+</script>
