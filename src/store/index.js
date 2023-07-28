48159bd34c9c2f37cbbcc5f816e872d083ac3ecc
Elizaveta Tsaregorodtseva <eitsaregorodtseva@miem.hse.ru>
2023-07-28 23:27:04 +0300

    ++

--- src/store/index.js
+++ src/store/index.js
@@ -1,33 +1,33 @@
-// import { createStore } from 'vuex';
-// import createPersistedState from "vuex-persistedstate";
+import { createStore } from 'vuex';
+import createPersistedState from "vuex-persistedstate";
 
 
-// export default createStore({
-//   state: {
-//     chosenOrganization: null,
-//     user: {
-//         id: null, 
-//         role: null, 
-//         organization_id: null, 
-//         el_signature: null
-//     }
-//   },
-//   mutations: {
-//     user(state) {
-//       state.user = {
-//         id: sessionStorage.getItem('user_id'), 
-//         role: sessionStorage.getItem('user_role'), 
-//         organization_id: sessionStorage.getItem('user_organization_id'), 
-//         el_signature: sessionStorage.getItem('user_el_signature')
-//       }
-//     },
-//     chooseOrganization(state, val) {
-//       state.chosenOrganization = val
-//     }
-//   },
-//   actions: {
-//   },
-//   modules: {
-//   },
-//   plugins: [createPersistedState()]
-// })
+export default createStore({
+  state: {
+    chosenOrganization: null,
+    user: {
+        id: null, 
+        role: null, 
+        organization_id: null, 
+        el_signature: null
+    }
+  },
+  mutations: {
+    user(state) {
+      state.user = {
+        id: sessionStorage.getItem('user_id'), 
+        role: sessionStorage.getItem('user_role'), 
+        organization_id: sessionStorage.getItem('user_organization_id'), 
+        el_signature: sessionStorage.getItem('user_el_signature')
+      }
+    },
+    chooseOrganization(state, val) {
+      state.chosenOrganization = val
+    }
+  },
+  actions: {
+  },
+  modules: {
+  },
+  plugins: [createPersistedState()]
+})
